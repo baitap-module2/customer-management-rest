fbde29953e3faa64725ac44419e44a83415d58f2
vietson6103@gmail.com <Hn0912388240>
2018-08-31 10:07:22 +0700

    add method show 1 customer

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -4,12 +4,10 @@ import com.codegym.model.Customer;
 import com.codegym.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,17 @@ public class CustomerController {
             return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<Customer>>(customers, HttpStatus.OK);
+    }
+
+    //    hiển thị 1 khách hàng
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id) {
+        System.out.println("Fetching Customer with id " + id);
+        Customer customer = customerService.findById(id);
+        if (customer == null) {
+            System.out.println("Customer with id " + id + " not found");
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
     }
 }
